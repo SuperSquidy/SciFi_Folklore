9dfc76bf42d5acc8d41ae367c7fcdcc0ae837c3f
Kaitlin Dell <kaitlinadell@gmail.com>
2016-09-16 05:57:34 -0400

    Putting SentenceSplitting Program on GitHub
    the second prototype of sentence splitting for walkthrough processing

---
+++ 0001Sentence_Splitting.py
@@ -0,0 +1,57 @@
+import random
+import os
+import fileinput
+
+#variables
+direct = 'Text_Files'
+rando = 'Generated_Files'
+walkthroughs = []
+texty = []
+allWalkthroughs = []
+
+
+#actually getting all the files
+filenames = os.listdir(direct)
+print filenames
+
+#opening, reading, and adding the files to 'walkthroughs' the files
+for f in filenames:
+    text_org = open(os.path.join(direct, f))
+    text = text_org.read()
+    text = text.split('.')
+    n = 0
+    for index in text:
+        text[n] = text[n].replace('\n',' ')
+        n+=1
+    walkthroughs.append(text)
+    text_org.close
+
+
+#flattening the list
+extend = allWalkthroughs.extend
+for l in walkthroughs:
+    extend(l) 
+
+#shuffling the list together
+random.shuffle(allWalkthroughs) 
+#print allWalkthroughs
+
+#writing the file
+already_there = os.listdir(rando)
+print already_there
+
+newName = raw_input(("Enter the new file name: "))
+
+while newName+'.txt' in already_there:
+    print '****File already exists****'
+    newName = str(raw_input(("Enter a different file name: ")))
+
+newWalkthrough = open(os.path.join(rando,newName+'.txt'), 'w')
+s = 0
+for g in allWalkthroughs:
+    newWalkthrough.write(allWalkthroughs[s])
+    s += 1    
+newWalkthrough.close()
+print 'File Saved.'
+
+
