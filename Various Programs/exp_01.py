8a906b40de393849fcf8ccc813001d2a7e265d70
Kaitlin Dell <kaitlinadell@gmail.com>
2016-09-28 17:08:44 -0400

    added so idea stuff for the code

--- Various Programs/exp_01.py
+++ Various Programs/exp_01.py
@@ -1,3 +1,11 @@
-input
-hidden_layer
-output
+#input--new information
+#hidden_layer--essentially the network's memory
+#output--what the program 'decides' the next thing will be based on the input and hidden
+
+#so basically, what I want to do overall is have a reccurent function which is
+#built to take the new input, 'add' it to the hidden layer, use the hidden layer
+#to help determine the output
+
+#want to teach the network a bunch of information and have it be able to predict the next word
+#when you give it a noun.
+#i.e. you give it the input 'you' and it decides the next most likely verb/word to come after
