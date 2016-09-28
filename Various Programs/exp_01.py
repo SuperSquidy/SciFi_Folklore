64961f16f818fe876bcd4ab5880956a28108d3ef
Kaitlin Dell <kaitlinadell@gmail.com>
2016-09-28 17:11:13 -0400

    actually added the comments
    replaced the code with a code i made in a separate file

--- Various Programs/exp_01.py
+++ Various Programs/exp_01.py
@@ -9,3 +9,22 @@
 #want to teach the network a bunch of information and have it be able to predict the next word
 #when you give it a noun.
 #i.e. you give it the input 'you' and it decides the next most likely verb/word to come after
+
+#in terms of a weighted matrix, the hidden layer will act as the reference for
+#decision of the most likely next word, and the hidden layer will change with each iteration
+#based on the LTSM logic 
+
+
+
+import numpy as np
+np.random.seed(0)
+
+#sigmoid curve used for probability
+# compute sigmoid nonlinearity
+def sigmoid(x):
+    output = 1/(1+np.exp(-x))
+    return output
+
+# convert output of sigmoid function to its derivative
+def sigmoid_output_to_derivative(output):
+    return output*(1-output)
