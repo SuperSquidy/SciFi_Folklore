537501f28ae38ecf44724cc0e9a9f3ff135a592f
Kaitlin Dell <kaitlinadell@gmail.com>
2016-10-01 19:49:24 -0400

    training text folder added, can split into words

--- Various Programs/exp_01.py
+++ Various Programs/exp_01.py
@@ -25,10 +25,16 @@
 #use ascii ord()--to number, chr()--to letter
 #then use bin to convert to binary, trim off the Ob, then converto to list and append to an array
 
-
-
+import random
+import os
+import fileinput
 import numpy as np
 np.random.seed(0)
+
+direct = 'Test_Texts'
+walkthroughs = []
+texty = []
+allWalkthroughs = []
 
 #sigmoid curve used for probability
 # compute sigmoid nonlinearity
@@ -39,3 +45,60 @@ def sigmoid(x):
 # convert output of sigmoid function to its derivative
 def sigmoid_output_to_derivative(output):
     return output*(1-output)
+
+def split_to_sent(raw):
+    texts = []
+    sentences = []
+
+    #actually getting all the files
+    filenames = os.listdir(raw)
+    #print filenames
+    
+    #opening, reading, and adding the files to 'walkthroughs' the files
+    for f in filenames:
+        text_org = open(os.path.join(raw, f))
+        text = text_org.read()
+        text = text.split('.')
+        n = 0
+        for index in text:
+            text[n] = text[n].replace('\n','')
+            n+=1
+        texts.append(text)
+        text_org.close
+    
+    extend = sentences.extend
+    for l in texts:
+        extend(l)
+    
+    return(sentences)
+
+def split_to_words(sent):
+    words = []
+    n = 0
+    for f in sent:
+        sent[n].split()
+        print(sent[n])
+        i = 0
+#        for w in sent[n]:
+#            words.append(sent[n[i]])
+#            i+=1
+#            print(words)
+        n+=1
+    return words
+
+def split_to_letters(words):
+    letters = []
+    n = 0
+    for f in words:
+        letters.extend(words[n])
+        n+=1
+    return(letters)
+    
+def make_words_binary(word):
+    binary = []    
+    return(binary)
+    
+doingThings = split_to_sent(direct)
+print(doingThings)
+doingThings_2 = split_to_words(doingThings)
+print(doingThings_2)
