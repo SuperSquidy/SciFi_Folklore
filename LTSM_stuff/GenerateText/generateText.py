a3130069f2405ae1025f41155d2456b930355241
Kaitlin Dell <kaitlinadell@gmail.com>
2017-02-25 16:57:04 -0500

    moved where weights are

--- LTSM_stuff/GenerateText/generateText.py
+++ LTSM_stuff/GenerateText/generateText.py
@@ -8,7 +8,7 @@ from keras.layers import LSTM
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
 # load ascii text and covert to lowercase
-filename = "wonderland.txt"
+filename = "TrainingBaby_03.txt"
 raw_text = open(filename).read()
 raw_text = raw_text.lower()
 # create mapping of unique chars to integers, and a reverse mapping
@@ -43,7 +43,7 @@ model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
 model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation='softmax'))
 # load the network weights
-filename = "weights-improvement-19-1.9435.hdf5"
+filename = "weights-improvement-19-1.9782.hdf5"
 model.load_weights(filename)
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 # pick a random seed
